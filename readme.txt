7830e1669050cd7ded5bbbbfe2854a1c4001c8bd
Arslan Ramay <arslanc104@gmail.com>
2019-03-02 15:52:16 +0500

    Initial commit

---
+++ readme.txt
@@ -0,0 +1,40 @@
+=== WooCommerce - Show Lowest Price Only in Variable Products ===
+Contributors: arslan104,woogenie
+Donate link: https://www.paypal.me/
+Tags: woocommerce, variations, variable products, woocommerce minimum price, prices, sale products, sales
+Requires at least: 4.0
+Tested up to: 5.1
+Stable tag: trunk
+License: GPLv2+
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Show only the lowest prices in WooCommerce variable products, and lowest sale price, instead the messy full range of prices and sales.
+
+
+== Description ==
+
+By default, WooCommerce shows full price range when it comes to variable products and there is no option in the <strong>Settings</strong> page to change that behavior or hide the Highest Price and display Only the Lowest Price.
+
+With this plugin, you can hide the Highest Price in variable products and just Show the Lowest Price to encourage more sales.
+
+<strong>This plugin has No options or settings</strong>, just activate the plugin and it’s done.
+
+<strong>Optional</strong>: You can hide the "From:" text by adding this CSS snippet to the 'Additional CSS' section of the WordPress Customizer or in your child theme's CSS file:
+<pre>span.woogenie-from-label { display: none; }</pre>
+
+== Installation ==
+
+1. Go to your WP Dashboard > Plugins and search for ‘show lowest price only in variable’ or…
+2. Download the plugin from WP repository.
+3. Upload the plugin folder to the '/wp-content/plugins/' directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Screenshots ==
+
+1. WooCommerce variable product price display before plugin activation.
+2. WooCommerce variable product price display after plugin activation.
+
+== Changelog ==
+
+= 0.9 =
+* Initial release
---
+++ woogenie-show-lowest-price-woo-variable-products.php
@@ -0,0 +1,74 @@
+<?php
+/*
+ * Plugin Name: WooCommerce - Show Lowest Price Only in Variable Products
+ * Plugin URI: https://woogenieplugins.com/
+ * Description: Shows only the Lowest Price in WooCommerce Variable Products. This plugin requires WooCommerce.
+ * Author: WooGenie Plugins
+ * Version: 0.9
+ * Author URI: https://woogenieplugins.com
+ * Text Domain: show-lowest-price-only-in-woocommerce-variable-products
+ * Domain Path: /languages
+ * WC requires at least: 3.0
+ * WC tested up to: 3.5.5
+ * License: GPLv2+
+*/
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+function show_lowest_price_only_in_woocommerce_variable_products_load_plugin_textdomain() {
+    load_plugin_textdomain( 'show-lowest-price-only-in-woocommerce-variable-products', FALSE, basename( dirname( __FILE__ ) ) . '/languages/' );
+}
+add_action( 'plugins_loaded', 'show_lowest_price_only_in_woocommerce_variable_products_load_plugin_textdomain' );
+
+add_filter( 'woocommerce_variable_sale_price_html', 'woogenie_variation_price_format', 10, 2 );
+add_filter( 'woocommerce_variable_price_html', 'woogenie_variation_price_format', 10, 2 );
+
+// For Variable products
+function woogenie_variation_price_format( $price, $product ) {
+
+    // Main Price
+    $prices = array( $product->get_variation_price( 'min', true ), $product->get_variation_price( 'max', true ) );
+	$price = $prices[0] !== $prices[1] ? sprintf( __( '<span class="woogenie-from-label">From: </span>%1$s', 'show-lowest-price-only-in-woocommerce-variable-products' ), wc_price( $prices[0] ) ) : wc_price( $prices[0] );
+	
+    // Sale Price
+    $prices = array( $product->get_variation_regular_price( 'min', true ), $product->get_variation_regular_price( 'max', true ) );
+    sort( $prices );
+	$saleprice = $prices[0] !== $prices[1] ? sprintf( __( '<span class="woogenie-from-label">From: </span>%1$s', 'show-lowest-price-only-in-woocommerce-variable-products' ), wc_price( $prices[0] ) ) : wc_price( $prices[0] );
+	
+    if ( $price !== $saleprice ) {
+        $price = '<del>' . $saleprice . '</del> <ins>' . $price . '</ins>';
+    }
+    return $price;
+}
+
+// For Grouped Products
+// Show product prices in WooCommerce 2.0 format
+add_filter( 'woocommerce_grouped_price_html', 'woogenie_grouped_price_format', 10, 2 );
+
+function woogenie_grouped_price_format( $price, $product ) {
+
+	$tax_display_mode = get_option( 'woocommerce_tax_display_shop' );
+	$child_prices     = array();
+
+	foreach ( $product->get_children() as $child_id ) {
+		$child_prices[] = get_post_meta( $child_id, '_price', true );
+	}
+
+	$child_prices     = array_unique( $child_prices );
+	$get_price_method = 'get_price_' . $tax_display_mode . 'uding_tax';
+
+	if ( ! empty( $child_prices ) ) {
+		$min_price = min( $child_prices );
+		$max_price = max( $child_prices );
+	} else {
+		$min_price = '';
+		$max_price = '';
+	}
+
+	if ( $min_price == $max_price ) {
+		$display_price = wc_price( $product->$get_price_method( 1, $min_price ) );
+	} else {
+		$from          = wc_price( $product->$get_price_method( 1, $min_price ) );
+		$display_price = sprintf( __( 'From: %1$s', 'show-only-lowest-prices-in-woocommerce-variable-products' ), $from );
+	}
+
+	return $display_price;
+}
